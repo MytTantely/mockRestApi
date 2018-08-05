9f4d22c39b3001bcccc62f203294f0ce69dcd1c6
Tantely Maharisoa <tantelymaharisoa@gmail.com>
2018-08-04 21:41:55 -0600

    CORS features

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,21 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 
 const indexRoutes = require('./routes/index');
 const companiesRoutes = require('./routes/companies');
+const clientRoutes = require('./routes/client');
 
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
+app.use(cors());
+
 app.use(bodyParser());
 app.use(indexRoutes.routes());
 app.use(companiesRoutes.routes());
+app.use(clientRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
